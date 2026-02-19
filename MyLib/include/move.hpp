1375a890679b1cdd97f6b07c10b222b8b1e4af42
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-02-19 13:37:12 +0100

    debug print

--- MyLib/include/move.hpp
+++ MyLib/include/move.hpp
@@ -37,6 +37,7 @@ constexpr Move::Move(Bitboard::Square from, Bitboard::Square to, MoveType type, 
   data_(from | to << 6 | type | piece) {
   assert(from >= Bitboard::a1 && from <= Bitboard::h8);
   assert(to >= Bitboard::a1 && to <= Bitboard::h8);
+  std::cout << type ;
   assert(type >= MoveType::standard && type <= MoveType::castle);
   assert(piece >= PromotionPiece::knight && piece <= PromotionPiece::queen);
 }
