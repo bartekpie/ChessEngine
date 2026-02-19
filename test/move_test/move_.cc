259453ab6a909302650fcb71c7e86fe119de0c1c
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-02-19 11:55:58 +0100

    adding constuctor tests

--- test/move_test/move_.cc
+++ test/move_test/move_.cc
@@ -2,11 +2,14 @@
 #include <iostream>
 #include "move.hpp"
 
-TEST(MoveTest, constructorTest) {
+TEST(MoveTest, valueConstructorTest) {
     Move m(static_cast<uint16_t>(1ULL << 2));
-    EXPECT_EQ(m.from(), 0x4);
+    EXPECT_EQ(m.data(), static_cast<uint16_t>(0x4));
 }
-
+TEST(MoveTest, constructorTest) {
+    Move m(Bitboard::Square(3 << 2), Bitboard::Square(3 << 2), promotion, queen );
+    EXPECT_EQ(m.data(), 0xE33);
+}
 TEST(MoveTest, fromToEncodingTest) {
     for (int i{}; i < 64; i++) {
       for (int j{63}; j>=0; j-- ){
@@ -15,6 +18,9 @@ TEST(MoveTest, fromToEncodingTest) {
         Move m(from, to);
         EXPECT_EQ(m.from(), from);
         EXPECT_EQ(m.to(), to);
+        EXPECT_EQ(m.type(), standard);
+        EXPECT_EQ(m.promotionType(), knight);
+
       }
     }
-}
+} 
