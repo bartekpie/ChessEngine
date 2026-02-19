e05e503cac16acb39e255b81dd4f211baf8a37b4
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-02-19 11:38:02 +0100

    adding test

--- test/move_test/move_.cc
+++ test/move_test/move_.cc
@@ -6,3 +6,15 @@ TEST(MoveTest, constructorTest) {
     Move m(static_cast<uint16_t>(1ULL << 2));
     EXPECT_EQ(m.from(), 0x4);
 }
+
+TEST(MoveTest, fromToEncodingTest) {
+    for (int i{}; i < 64; i++) {
+      for (int j{63}; j>=0; j++ ){
+        Bitboard::Square from = static_cast<Bitboard::Square>(i);
+        Bitboard::Square to = static_cast<Bitboard::Square>(j);
+        Move m(from, to);
+        EXPECT_EQ(m.from(), from);
+        EXPECT_EQ(m.to(), to);
+      }
+    }
+}
