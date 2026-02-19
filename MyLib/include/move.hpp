f2956c8593efba54e59cc070f009d7693996f931
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-02-19 10:57:00 +0100

    changing methods implemantaion place into hpp file thats better practise for efficiency

--- MyLib/include/move.hpp
+++ MyLib/include/move.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include "bitboard.hpp"
 
@@ -8,20 +9,43 @@ enum MoveType : uint16_t{
     castle = 3 << 12
 };
 enum PromotionPiece : uint16_t{
-    none, 
-    knight = 1 << 14,
-    bishop = 2 << 14,
+    knight,
+    bishop = 1 << 14,
+    rook = 2 << 14,
     queen = 3 << 14
 };
-class move {
+class Move {
     private :
       uint16_t data_;
     public :
-      move (Bitboard::Square from, Bitboard::Square to, MoveType type) : 
-        data_((from | to << 6 | type)) {}
-      MoveType type();
-      Bitboard::Square from();
-      Bitboard::Square to();
-      static constexpr move makeMove(Bitboard::Square from, Bitboard::Square to, MoveType type = standard, PromotionPiece piece = none);
+      constexpr Move(uint16_t move);
+      constexpr Move(Bitboard::Square from, Bitboard::Square to, MoveType type = standard, PromotionPiece = knight); 
+        
+      constexpr MoveType type() const;
+      constexpr PromotionPiece promotionType() const;
+      constexpr Bitboard::Square from()const;
+      constexpr Bitboard::Square to()const;
+      static constexpr Move makeMove(Bitboard::Square from, Bitboard::Square to, MoveType type = standard, PromotionPiece piece = knight);
 
 };
+constexpr Move::Move(uint16_t move) : 
+  data_(move){}
+
+constexpr Move::Move(Bitboard::Square from, Bitboard::Square to, MoveType type = standard, PromotionPiece piece = knight) : 
+  data_(from | to << 6 | type | piece) {}
+
+constexpr MoveType Move::type() const{
+    return( MoveType(data_  & 0x3000) );
+}
+constexpr PromotionPiece Move::promotionType() const{
+    return PromotionPiece(data_ & 0xC000);
+}
+constexpr Bitboard::Square Move::from() const{
+    return Bitboard::Square(data_ & 0x3f);
+}
+constexpr Bitboard::Square Move::to() const{
+    return Bitboard::Square((data_ >> 6) & 0x3f);
+}
+constexpr Move Move::makeMove(Bitboard::Square from, Bitboard::Square to, MoveType type = standard, PromotionPiece piece = knight) {
+    return Move(from, to, type, piece);
+}
