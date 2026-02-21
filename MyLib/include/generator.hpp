185a06154861241ad65322be06809a64e48f3cfb
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-02-21 12:18:46 +0100

    added precompiled knight table

--- MyLib/include/generator.hpp
+++ MyLib/include/generator.hpp
@@ -34,3 +34,27 @@ enum Pieces : uint8_t { Pawn = 0, Knight, Bishop, Queen, King};
 
 template<Pieces>
 void generate_moves(const Position& position, MoveList& list);
+
+constexpr Bitboard::bitboard notAFile  = 0xfefefefefefefefeULL;
+constexpr Bitboard::bitboard notABFile = 0xfcfcfcfcfcfcfcfcULL;
+constexpr Bitboard::bitboard notHFile  = 0x7f7f7f7f7f7f7f7fULL;
+constexpr Bitboard::bitboard notGHFile = 0x3f3f3f3f3f3f3f3fULL;
+
+constexpr std::array<Bitboard::bitboard, 64> knight_precompiled = []() constexpr  
+{
+   std::array<Bitboard::bitboard, 64> moves {};
+   Bitboard::bitboard current {};
+   for (uint8_t square{0}; square < 64; square++) {
+     current = 1ULL << square;
+
+     moves[square] |= (current << 17) & notAFile;
+     moves[square] |= (current << 15) & notHFile;
+     moves[square] |= (current << 10) & notABFile;
+     moves[square] |= (current << 6)  & notGHFile;
+     moves[square] |= (current >> 17) & notHFile;
+     moves[square] |= (current >> 15) & notAFile;
+     moves[square] |= (current >> 10) & notGHFile;
+     moves[square] |= (current >> 6)  & notABFile;
+   } 
+   return moves;
+}();
