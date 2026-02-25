a6b36724c88de4f06750351341169bd15008eddb
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-02-25 19:08:16 +0100

    adding the FEN standard to positon and easy testing

--- MyLib/include/position.hpp
+++ MyLib/include/position.hpp
@@ -1,5 +1,8 @@
 #include "bitboard.hpp"
 #include <array>
+#include <string>
+#include <cassert>
+#include <unordered_map>
 enum class PiecesType {
     pawn= 0 , knight, bishop, rook, queen, king
 };
@@ -18,6 +21,8 @@ class Position {
       Bitboard::bitboard emptySpaces_;
       Color sideToMove ;
     public :
+      void loadFromFEN(std::string fen_position);
+      void clear();
       Bitboard::bitboard getPieces(Pieces piece) const {return board_[int(piece)];}
       template <Color color> Bitboard::bitboard getPiecesByColor(PiecesType piece) const;
       template <PiecesType piece> Bitboard::bitboard getOurs() const;
@@ -40,9 +45,65 @@ Bitboard::bitboard Position::getOurs() const {
     int offset = sideToMove == Color::black ? 0 : 6;
     return board_[int(piece) + offset];
 }
-inline Bitboard::bitboard Position::getOurs() const {
-  auto r = sideToMove == Color::white ? whitePieces_ : blackPieces_;
-  return r;
+inline void Position::clear()
+{
+    whitePieces_ = 0ULL;
+    blackPieces_ = 0ULL;
+    emptySpaces_ = ~0ULL;
+    board_.fill(0ULL);
+}
+inline void Position::loadFromFEN(std::string fen_position)
+{
+    clear();
+    auto rank{7};
+    auto file{0};
+    auto index{0};
+    std::unordered_map<char, std::pair<Pieces, Color>> charToPiece {
+        {'P', {Pieces::white_pawn,   Color::white}},
+        {'N', {Pieces::white_knight, Color::white}},
+        {'B', {Pieces::white_bishop, Color::white}},
+        {'R', {Pieces::white_rook,   Color::white}},
+        {'Q', {Pieces::white_queen,  Color::white}},
+        {'K', {Pieces::white_king,   Color::white}},
+        {'p', {Pieces::black_pawn,   Color::black}},
+        {'n', {Pieces::black_knight, Color::black}},
+        {'b', {Pieces::black_bishop, Color::black}},
+        {'r', {Pieces::black_rook,   Color::black}},
+        {'q', {Pieces::black_queen,  Color::black}},
+        {'k', {Pieces::black_king,   Color::black}}
+    };
+    for (char piece: fen_position) {
+        if (piece == ' '){
+            break;
+        }
+        else if (piece == '/'){
+            rank--;
+            file = 0;
+        } 
+        else if(isdigit(piece)){
+            file += piece - '0';
+        }
+        else {
+            assert(charToPiece.find(piece) == charToPiece.end());
+            auto [piecetype, color] = charToPiece[piece];
+            auto position = Bitboard::Square(rank*8 + file);
+            Bitboard::set_bit(board_[piecetype], position);
+            whitePieces_ |= color == Color::white ? 1ULL << position : 0ULL;
+            blackPieces_ |= color == Color::black ? 1ULL << position : 0ULL;
+            emptySpaces_ &= ~(1ULL << position);
+            file++;
+        }
+        index++;
+        
+    }
+    if( ++index < fen_position.length())
+      sideToMove = fen_position[index] == 'w' ? Color::white : Color::black;
+
+}
+inline Bitboard::bitboard Position::getOurs() const
+{
+    auto r = sideToMove == Color::white ? whitePieces_ : blackPieces_;
+    return r;
 }
 template <PiecesType piece>
 Bitboard::bitboard Position::getOpponents() const {
