c508803a5331c61b1f8acb16c0326b1b72d529d6
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-02-25 10:33:56 +0100

    adding tests

--- test/generator_test/gen.cc
+++ test/generator_test/gen.cc
@@ -10,7 +10,7 @@ TEST(MoveListTest, AddsMovesCorrectly) {
     list += m2;
 
     int count = 0;
-    for (auto& move : list) {
+    for (const auto& move : list) {
         if (count == 0)
           EXPECT_EQ(move.data(), m1.data());
         if (count == 1)
@@ -44,4 +44,29 @@ TEST(MoveListTest, IterationAfterReset) {
     }
     EXPECT_EQ(count, 0);
     EXPECT_EQ(list.begin(), list.end());
+};
+TEST(MoveListTest, AddingAfterReset) {
+    MoveList list;
+    Move m1 = Move::makeMove(Bitboard::a1, Bitboard::a2);
+    Move m2 = Move::makeMove(Bitboard::h7, Bitboard::h8, MoveType::promotionBishop);
+    
+    list += m1;
+    list += m2;
+    list.reset();
+    Move m3 = Move::makeMove(Bitboard::g2, Bitboard::g2);
+    Move m4 = Move::makeMove(Bitboard::h2, Bitboard::g8, MoveType::promotionQueen);
+    list += m3;
+    list += m4;
+    EXPECT_EQ(list.begin(), m3);
+    EXPECT_EQ(list.begin() + 1, m4);
+};
+TEST(MoveListTest, AddingAfterMaxCapacity) {
+    MoveList list;
+    for (int i{}; i < 256; i++) 
+      list += Move::makeMove(Bitboard::a1, Bitboard::a2);
+   
+    #ifndef NDEBUG
+        EXPECT_DEATH(list += Move::makeMove(Bitboard::a1, Bitboard::a2);, "");
+    #endif
+
 };
