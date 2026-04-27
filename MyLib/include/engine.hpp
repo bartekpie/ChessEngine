4bd0c637eef2ad3961d9f1592260063b0c18a439
Bartek Pietruczuk <pietruczukbartek@gmail.com>
2026-04-27 21:16:20 +0200

    added new funciondeclaration  that prints Statisctics

--- MyLib/include/engine.hpp
+++ MyLib/include/engine.hpp
@@ -2,24 +2,27 @@
 #include "position.hpp"
 #include "generator.hpp"
 #include "move.hpp"
+#include <iostream>
 #include <atomic>
 #include <limits>
 #include <chrono>
+#include <thread>
 
 
 namespace Engine {
     enum playerType { min, max };
-	class engine
-	{
+	class engine {
+	private:
+		Position position_{};
+		MoveList moveList_{};
+		std::atomic<bool> isSearching_{false};
+		std::atomic<std::int64_t> nodesSearched_{0};
 	public:
 		int evalulate();
-		template <playerType player> int search(int depth, int alfa, int beta);
+		template <playerType player> int search(int depth, int alfa = std::numeric_limits<int>(min), int beta = std::numeric_limits<int>(max));
 		Move bestMove(int depth, int alfa, int beta);
-	private:
-		Position position_;
-		MoveList moveList_;
-		std::atomic<bool> stopFlag_{false};
-		std::int64_t nodesSearched_{0};
+		void printStatistics() const ;
+	
 	};
 
 }
